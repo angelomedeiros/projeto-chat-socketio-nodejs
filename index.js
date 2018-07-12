976bacc7abd70862762b0c19c99150db42772bba
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-07-12 13:35:46 -0300

    hello world usando o express

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-var http = require('http')
+// var http = require('http')
 
-http
-  .createServer( (req, res) => {
-    res.writeHead( 200, { 'Content-type': 'text/plain' } )
-    res.end( 'Hello world!' )
-  })
-  .listen(3000)
+// http
+//   .createServer( (req, res) => {
+//     res.writeHead( 200, { 'Content-type': 'text/plain' } )
+//     res.end( 'Hello world!' )
+//   })
+//   .listen(3000)
 
-console.log('Server started!')
+// console.log('Server started!')
+
+var express = require('express')
+var app = express()
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+// app.listen(3000) Isso já irá fazer funcionar!!
+var port = 3000
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+})
