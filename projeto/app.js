8c9d98a61573440f7b80cfeebc50a22ac965e1ec
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-07-12 17:49:48 -0300

    Config do ambiente de desenvolvimento

---
+++ projeto/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const path    = require('path')
+const app     = express()
+
+const env = path.join(__dirname, './src/configs/env/', process.env.NODE_ENV || 'development')
+
+require(env)(app)
+
+app.listen(app.get('port'), app.get('host'), () => {
+  console.log(`Server listening on host:port... ${app.get('host')}:${app.get('port')}`)
+})
---
+++ projeto/src/configs/env/development.js
@@ -0,0 +1,31 @@
+const bodyParser       = require('body-parser')
+const expressSession   = require('express-session')
+const expressValidator = require('express-validator')
+const hbs              = require('express-hbs')
+const methodOverride   = require('method-override')
+const morgan           = require('morgan')
+const path             = require('path')
+
+module.exports = (app) => {
+  app.set('port', 9000)
+  app.set('host', '127.0.0.1')
+  app.set('views', path.join(__dirname, './../../../build/views'))
+  app.set('view engine', 'hbs')
+
+  app.use(morgan('dev'))
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extend: false }))
+  app.use(methodOverride('_method'))
+  app.use(expressSession({
+    secret: '&8&6asdgbu77as_=-asddas{}',
+    resave: false,
+    saveUninitialized: false
+  }))
+  app.use(expressValidator())
+
+  app.engine('hbs', hbs.express4({
+    defaultLayout: path.join(app.get('views'), 'layout/main.hbs'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    layoutsDir: path.join(app.get('views'), 'layouts')
+  }))
+}
