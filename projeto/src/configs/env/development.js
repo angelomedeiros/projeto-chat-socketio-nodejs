d905a95e77c9b7e9a2732d233bb99843d8426fef
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-07-13 01:05:26 -0300

    Config do mongo

--- projeto/src/configs/env/development.js
+++ projeto/src/configs/env/development.js
@@ -1,11 +1,12 @@
 const bodyParser       = require('body-parser')
+const express          = require('express')
 const expressSession   = require('express-session')
 const expressValidator = require('express-validator')
 const hbs              = require('express-hbs')
 const methodOverride   = require('method-override')
+const mongoose         = require('mongoose')
 const morgan           = require('morgan')
 const path             = require('path')
-const express          = require('express')
 
 module.exports = (app) => {
   app.set('port', 9000)
@@ -13,6 +14,10 @@ module.exports = (app) => {
   app.set('views', path.join(__dirname, './../../../build/views'))
   app.set('view engine', 'hbs')
   app.set('assets', path.join(__dirname, './../../../build'))
+  app.set('mongo_host', '127.0.0.1')
+  app.set('mongo_port', 27017)
+  app.set('mongo_db', 'chatangelo')
+  app.set('mongo_url', `mongodb://${app.get('mongo_host')}:${app.get('mongo_port')}/${app.get('mongo_db')}`)
 
   app.use(express.static(app.get('assets')))
   app.use(morgan('dev'))
@@ -31,4 +36,6 @@ module.exports = (app) => {
     partialsDir: path.join(app.get('views'), 'partials'),
     layoutsDir: path.join(app.get('views'), 'layouts')
   }))
+
+  mongoose.connect(app.get('mongo_url'))
 }
