c1661b2484508be9384e3bea6829fd8edeaa29fc
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-07-17 22:23:50 -0300

    Add o titulo dos canais do chat dinamicamente

--- webpapp/build/js/app.js
+++ webpapp/build/js/app.js
@@ -1,26 +1,51 @@
-var socket = io('//localhost:3000')
+$(document).ready(() => {
+  (function() {
+    var getRooms = () => {
+      return $.get('http://localhost:3000/rooms', data => {
+        if (!data.status) {
+          return
+        }
 
-$('#message').keypress( e => {
-  if (e.which == 13) {
-    var val = $('#message').val()
-    console.log('Val ' + val)
+        var titleTemplate = 
+              ' <li class="list-group-item title"> ' +
+              '   <h4>Canais (' + data.rooms.length + ')</h4> ' +
+              ' </li> '
 
-    socket.emit('message', {
-      message: val
-    })
+        $('.channels').append(titleTemplate)
 
-    e.preventDefault() // ou return false
-  }
+      })
+    }
+
+    getRooms()
+  })()
+
+  var socket = io('//localhost:3000')
+
+  $('#message').keypress( e => {
+    if (e.which == 13) {
+      var val = $('#message').val()
+      console.log('Val ' + val)
+
+      socket.emit('message', {
+        message: val
+      })
+
+      e.preventDefault() // ou return false
+    }
+  })
+
+  socket.on('message', data => {
+    var template = 
+      ' <div class="col-xs-12 message"> ' + 
+      '   <div class="avatar col-xs-6 col-md-1"> ' +
+      '     <h2>A</h2> ' +
+      '   </div> ' +
+      '   <p class="text col-xs-6 col-md-11">' + data.message + '</p> ' +
+      ' </div> '
+
+    $('.conversation').append(template)
+  })
 })
 
-socket.on('message', data => {
-  var template = 
-    ' <div class="col-xs-12 message"> ' + 
-    '   <div class="avatar col-xs-6 col-md-1"> ' +
-    '     <h2>A</h2> ' +
-    '   </div> ' +
-    '   <p class="text col-xs-6 col-md-11">' + data.message + '</p> ' +
-    ' </div> '
 
-  $('.conversation').append(template)
-})
+
