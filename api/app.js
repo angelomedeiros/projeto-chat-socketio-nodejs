2bca740a582106528bd28b9b8672b380dabebf8d
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-07-17 22:08:51 -0300

    Liberando o cors

--- api/app.js
+++ api/app.js
@@ -4,9 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose')
+var cors = require('cors')
 
-var indexRouter = require('./routes');
-var usersRouter = require('./routes/users');
+// var indexRouter = require('./routes');
+// var usersRouter = require('./routes/users');
 
 var app = express();
 var server = require('http').Server(app)
@@ -20,6 +21,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors())
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
@@ -36,9 +38,8 @@ io.on('connection', (socket) => {
     socket.emit('message', {
       message: data.message.toUpperCase()
     })
-    
-  })
 
+  })
 
 })
 
